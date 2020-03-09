d8e55c4d1d284ee6a316b1325168da346c72f1a9
que master <quoeamaster@gmail.com>
2020-03-09 12:31:19 +0800

    20200309a

--- 2020-03-gui-task-app/app/App.go
+++ 2020-03-gui-task-app/app/App.go
@@ -20,18 +20,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 const defaultRepoLocation = "other_resources/notes.json"
 
 
 type App struct {
-	notes map[string]interface{}
+	// map [string] -> list of map [string] -> interface (any value)
+	notes map[string][]map[string]interface{}
 }
 
 func NewApp() (appPtr *App) {
 	appPtr = new(App)
-	appPtr.notes = make(map[string]interface{})
+	appPtr.notes = make(map[string][]map[string]interface{})
 
 	err := appPtr.loadNotesFromRepo()
 	if err != nil {
@@ -89,15 +91,94 @@ func (n *App) OnStart() (err error) {
  */
 func (n *App) OnCreateNoteTask(content, todayInString string) (err error) {
 	//fmt.Println("tbd - save the note / task:", content, todayInString)
+	var listOfMap []map[string]interface{}
 	listing := n.notes[todayInString]
+
+	// try / catch on ANY error happening later
+	defer func() {
+		if r := recover(); r != nil {
+			err = r.(error)
+		}
+	}()
+
 	if listing == nil {
-		// TODO: create an array of interface{}
+		// create an array of interface{}
+		listOfMap = make([]map[string]interface{}, 0)
 
 	} else {
-		// append
-		// TODO: convert the object to array of interface{}
+		// convert the object to array of map[string]interface{}
+		//listOfMap = listing.([]map[string]interface{}) (no need for conversion... probably)
+		listOfMap = listing
 	}
+	// append
+	entry := make(map[string]interface{})
+	entry["content"] = content
 
+	listOfMap = append(listOfMap, entry)
+	n.notes[todayInString] = listOfMap
+
+	// write to file
+	notesInString := n.notesRepoToString(n.notes)
+	err = ioutil.WriteFile(defaultRepoLocation, []byte(notesInString), 0644)
+	
+	return
+}
+
+func (n *App) notesRepoToString(repo map[string][]map[string]interface{}) (value string) {
+	var b strings.Builder
+
+	fmt.Fprintf(&b, "{ ")
+	for key, val := range repo {
+		fmt.Fprintf(&b, "\"%v\": [", key)
+		// list of map (notes content)
+		for idx, noteObject := range val {
+			if idx > 0 {
+				fmt.Fprintf(&b, ", ")
+			}
+			fmt.Fprintf(&b, n.noteToString(noteObject, true))
+		} // end -- for ( [] -> map[string]interface{} level)
+		fmt.Fprintf(&b, "]")
+	} // end -- for ( date -> []map[string]interface{} level)
+
+	fmt.Fprintf(&b, " }")
+	value = b.String()
+
+	return
+}
+
+/**
+ *	convert the note (map[string]interface{}) to string format
+ */
+func (n *App) noteToString(entry map[string]interface{}, needWrapping bool) (value string) {
+	//[]byte(value)
+
+	var b strings.Builder
+	idx := 0
+
+	if needWrapping {
+		fmt.Fprint(&b, "{ ")
+	}
+	for key, val := range entry {
+		if idx > 0 {
+			fmt.Fprintf(&b, ", ")
+		}
+		// key part
+		fmt.Fprintf(&b, "\"%v\": ", key)
+		// value part
+		switch val.(type) {
+		case int, int8, int16, int32, int64:
+			fmt.Fprintf(&b, "%v", val)
+		case float32, float64:
+			fmt.Fprintf(&b, "%v", val)
+		default:
+			fmt.Fprintf(&b, "\"%v\"", val)
+		}
+		idx++
+	}
+	if needWrapping {
+		fmt.Fprint(&b, " }")
+	}
+	value = b.String()
 
 	return
 }
