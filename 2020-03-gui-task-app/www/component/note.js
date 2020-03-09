3c0058a8b6f0b47e854a69d8bbb4622e0ef15318
que master <quoeamaster@gmail.com>
2020-03-09 16:21:49 +0800

    20200309c

---
+++ 2020-03-gui-task-app/www/component/note.js
@@ -0,0 +1,10 @@
+
+Vue.component('note', {
+    props: ['note'],
+    template: `
+<div>
+    check the positioning based on https://www.tutorialspoint.com/css/css_positioning.htm
+    {{note}} hi there
+</div>
+`
+})
